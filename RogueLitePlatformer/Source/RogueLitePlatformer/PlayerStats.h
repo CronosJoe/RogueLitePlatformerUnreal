10dd755a60e4492637aa566b36d21e6842c1e004
CronosJoe <CronosJoe@gmail.com>
2022-01-28 14:08:17 -0800

    fixed up movement and removed bad animations

---
+++ RogueLitePlatformer/Source/RogueLitePlatformer/PlayerStats.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "PlayerStats.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUELITEPLATFORMER_API APlayerStats : public APlayerState
+{
+	GENERATED_BODY()
+protected:
+public:
+private:
+
+};
